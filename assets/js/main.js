f89b7d1151a72f692b12f66233c063b2723229aa
dotNameMush <74607573+dotNameMush@users.noreply.github.com>
2021-12-06 16:57:29 +0800

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function no(){
+    changeposx = Math.floor(Math.random() * 201) -100;
+    changeposy = Math.floor(Math.random() * 201) -100;
+    document.getElementById('no').style.top = changeposy + 'px';
+    document.getElementById('no').style.right = changeposx + 'px';
+}
+function yes(){
+    document.getElementById('h1').innerHTML = "I thought so!😂😂😂";
+}
